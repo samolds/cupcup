9dcdc109f657715bd9a7a99f36669a5cbff1b1df
Sam Olds <samolds@gmail.com>
2016-03-14 12:42:24 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CupCupClub
+This is just a silly static site where I'm playing around with [P5](http://p5js.org) with a
+photo of my dog.
+
+### Continue to the [GitHub Pages Site](http://samolds.github.io/cupcup) for this project.
---
+++ js/custom.js
@@ -0,0 +1,22 @@
+function setup() {
+  port = createCanvas(640, 480);
+  port.parent('viewport');
+  port.parent('viewport');
+
+  loadImage('/img/nathan.png', function(img) {
+    image(img, 0, 0);
+  });
+
+  loadImage('/img/natezilla.png', function(img) {
+    image(img, 0, 0);
+  });
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
