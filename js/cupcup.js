689d65ec246cec5a05f272c29c90bc9dae28cd44
Sam Olds <samolds@gmail.com>
2016-03-16 22:43:24 -0600

    added some photos of henry

--- js/cupcup.js
+++ js/cupcup.js
@@ -23,64 +23,89 @@ var Globals = {
   possibleShapes: ["ellipse", "triangle", "quad"],
   possibleBackgrounds: [                  // (width x height)
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/bus.png",
+                  path:         "/cupcup/img/master/bus.png",
                   nickname:     "bus"
                 },
                 { heightRatio:  1.500000, // (500 x 750)
-                  path:         "/cupcup/img/butters.png",
+                  path:         "/cupcup/img/master/butters.png",
                   nickname:     "butters"
                 },
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/captain.png",
+                  path:         "/cupcup/img/master/captain.png",
                   nickname:     "captain"
                 },
                 { heightRatio:  1.269231, // (650 x 825)
-                  path:         "/cupcup/img/captainpancake.png",
+                  path:         "/cupcup/img/master/captainpancake.png",
                   nickname:     "captainpancake"
                 },
                 { heightRatio:  1.000000, // (600 x 600)
-                  path:         "/cupcup/img/cupcup.png",
+                  path:         "/cupcup/img/master/cupcup.png",
                   nickname:     "cupcup"
                 },
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/face.png",
+                  path:         "/cupcup/img/master/face.png",
                   nickname:     "face"
                 },
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/littleewok.png",
+                  path:         "/cupcup/img/master/littleewok.png",
                   nickname:     "littleewok"
                 },
                 { heightRatio:  1.500000, // (500 x 750)
-                  path:         "/cupcup/img/natjan.png",
-                  nickname:     "natjan"
+                  path:         "/cupcup/img/master/nugget.png",
+                  nickname:     "nugget"
                 },
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/pancake.png",
+                  path:         "/cupcup/img/master/pancake.png",
                   nickname:     "pancake"
                 },
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/poop.png",
+                  path:         "/cupcup/img/master/poop.png",
                   nickname:     "poop"
                 },
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/poptart.png",
+                  path:         "/cupcup/img/master/poptart.png",
                   nickname:     "poptart"
                 },
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/rat.png",
+                  path:         "/cupcup/img/master/rat.png",
                   nickname:     "rat"
                 },
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/shithead.png",
+                  path:         "/cupcup/img/master/shithead.png",
                   nickname:     "shithead"
                 },
                 { heightRatio:  1.500000, // (500 x 750)
-                  path:         "/cupcup/img/sir.png",
+                  path:         "/cupcup/img/master/sir.png",
                   nickname:     "sir"
                 },
                 { heightRatio:  0.666667, // (750 x 500)
-                  path:         "/cupcup/img/turd.png",
+                  path:         "/cupcup/img/master/turd.png",
                   nickname:     "turd"
+                },
+
+                { heightRatio:  1.000000, // (600 x 600)
+                  path:         "/cupcup/img/squire/a.png",
+                  nickname:     "a"
+                },
+                { heightRatio:  1.000000, // (600 x 600)
+                  path:         "/cupcup/img/squire/b.png",
+                  nickname:     "b"
+                },
+                { heightRatio:  0.666667, // (750 x 500)
+                  path:         "/cupcup/img/squire/c.png",
+                  nickname:     "c"
+                },
+                { heightRatio:  1.000000, // (600 x 600)
+                  path:         "/cupcup/img/squire/d.png",
+                  nickname:     "d"
+                },
+                { heightRatio:  1.000000, // (600 x 600)
+                  path:         "/cupcup/img/squire/e.png",
+                  nickname:     "e"
+                },
+                { heightRatio:  0.666667, // (750 x 500)
+                  path:         "/cupcup/img/squire/fonzie.png",
+                  nickname:     "fonzie"
                 },
               ],
 }
